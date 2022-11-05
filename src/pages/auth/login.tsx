493e6b59e79ab916f7cd1d8ec3bf6fa5dbb0c594
Qxtlp <xi.tulop@yandex.com>
2022-11-05 19:33:04 +0700

    add login page

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+export default function Home({}: Props) {
+  return (
+    <main data-theme="dark">
+      <header>
+        Hearder
+        <nav></nav>
+      </header>
+      <article>Article</article>
+      <footer>Footer</footer>
+    </main>
+  );
+}
